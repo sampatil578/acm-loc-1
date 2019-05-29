fcab3d08772fd92e733be173799a40564effa85a
ankitchandkaushik <ankit.kaushik.33671@gmail.com>
2019-05-29 16:11:39 +0530

    Assignment 2 added

---
+++ Ankit Chand/Assignment 2/gk.js
@@ -0,0 +1,139 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "World Health day is observed on?",
+    options: ["3rd April", "4th April", "5th April", "7th April"],
+    answer: 3
+  }, {
+    question: "For his major role in the development of computer chip 'Pentium', which Indian IT expert is called the 'Father of Pentium' ?",
+    options: ["Ajay Bhatt", "Anand Chandrasekhar", "Vinod Dham", "Bishwamohan Pani"],
+    answer: 2
+  }, {
+    question: "Non Metal that remains liquid at room temprature is",
+    options: ["Phophorous", "Bromine", "Clorine","Helium"],
+    answer: 1
+  },{
+    question: "Which of the following European countries is known as the 'Land of a thousand lakes' ?",
+    options: ["Finland", "Norway", "Sweden", "Estonia"],
+    answer: 0
+  }, {
+    question: "'Stilwell Road' connects India with which among the following neighbors?",
+    options: ["China", "Mymmar", "Pakistan", "Bhutan"],
+    answer: 0
+  }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+          alert('You didn\'t selected any option !');
+          quesCounter++;
+          nextQuestion();
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
+    {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+   
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
+
+
---
+++ Ankit Chand/Assignment 2/got.js
@@ -0,0 +1,139 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "In the TV show, what was Hodor called before he got his tragic door-holding nickname?",
+    options: ["Wylis", "Horys", "Myrys", "Gladys"],
+    answer: 0
+  }, {
+    question: "Dany's dragons are (or were) called Drogon, Viserion and ____?",
+    options: ["Dougal", "Vhagar", "Rhaegal", "Balerion"],
+    answer: 2
+  }, {
+    question: "Which relative did Euron Greyjoy murder to take the Salt Throne of the Iron Islands?",
+    options: ["His uncle", "His brother", "His nephew","His cousin"],
+    answer: 1
+  },{
+    question: "What is Arya's sword name?",
+    options: ["Ice", "Pointy", "Fang", "Needle"],
+    answer: 3
+  }, {
+    question: "In which King's Landing slum did Gendry grow up?",
+    options: ["Rat Bottom", "Flea Bottom", "Dragonpit", "Rhaneny's Hill"],
+    answer: 1
+    }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+          alert('You didn\'t selected any option !');
+          quesCounter++;
+          nextQuestion();
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
+    {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+   
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
+
+
