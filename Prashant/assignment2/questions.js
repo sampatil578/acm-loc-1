4089b8e8d4f56430dfb855c11d3dc06e61af7945
18JE0618 <50778193+18JE0618@users.noreply.github.com>
2019-05-29 14:15:22 +0530

    Add files via upload

---
+++ Prashant/assignment2/questions.js
@@ -0,0 +1,62 @@
+var questions = [{
+    "question": "Which one of these is not a way you can be out in cricket?",
+    "option1": "Bailed",
+    "option2": "Bowled",
+    "option3": "stumped",
+    "option4": "Caught",
+    "answer": "1" },{
+    "question": "On what surface is cricket traditionally played?",
+    "option1": "ice",
+    "option2": "Astroturf",
+    "option3": "Grass",
+    "option4": "sand",
+    "answer": "3" },{
+    "question": "How many balls or deliveries are bowled in one over?",
+    "option1": "2",
+    "option2": "22",
+    "option3": "30",
+    "option4": "6",
+    "answer": "4"},{
+    "question": "Which two countries compete for cricket's Ashes?",
+    "option1": "India & Pakistan",
+    "option2": "England & Australia",
+    "option3": "Mexico & Brazil",
+    "option4": "Cook Islands & Turkey",
+    "answer": "2" },{
+    "question": "If a batsman is out on the very first ball he faced, what is it called?",
+    "option1": "Golden Fleece",
+    "option2": "Golden Duck",
+    "option3": "Mallard Duck",
+    "option4": "Elephant Duck",
+    "answer": "2" },{
+    "question": "What does the term LBW stand for in cricket?",
+    "option1": "Let's Behave Wickedly",
+    "option2": "Lift Bat and Whack",
+    "option3": "Leg Before Wicket",
+    "option4": "Launch Ball Wicketwards",
+    "answer": "3" },{
+    "question": "Which of these is not a fielding position in cricket?",
+    "option1": "Silly Mid On",
+    "option2": "Winded Willow",
+    "option3": "Fine Leg",
+    "option4": " Second Slip",
+    "answer": "2" },{
+    "question": "What is the name given to the two people who officiate in a cricket match?",
+    "option1": "Judges",
+    "option2": "Umpires",
+    "option3": "Referee",
+    "option4": "dada",
+    "answer": "2" },{
+    "question": "Which colour of cricket ball has not been used in an official cricket match?",
+    "option1": "pink",
+    "option2": "red",
+    "option3": "white", 
+    "option4": "green",
+    "answer": "4" },{
+    "question": "Who is the god of cricket?",
+    "option1": "Googly",
+    "option2": "sachin",
+    "option3": "dhoni",
+    "option4": "kapil",
+    "answer": "2" }
+    ];
