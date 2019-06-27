0d17d434f254454614cc2bd4702a4cadd7f901fa
samruddha patil <45798222+sampatil578@users.noreply.github.com>
2019-06-28 05:08:38 +0530

    Add files via upload

---
+++ Samruddha/Project#1/bb.js
@@ -0,0 +1,199 @@
+function myFunction(m){
+
+	var n3 = document.getElementById("n3");
+	var n2 = document.getElementById("n2");
+	var n1 = document.getElementById("n1");
+
+	if(m===1){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "none";
+		n3.getElementsByClassName("n3")[1].style.display = "block";
+		n3.getElementsByClassName("n3")[2].style.display = "block";
+		n3.getElementsByClassName("n3")[3].style.display = "block";
+		n3.getElementsByClassName("n3")[4].style.display = "block";
+		n3.getElementsByClassName("n3")[5].style.display = "block";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[1].style.display = "none";
+		n2.getElementsByClassName("vd")[2].style.display = "none";
+		n2.getElementsByClassName("vd")[3].style.display = "none";
+		n2.getElementsByClassName("vd")[4].style.display = "none";
+		n2.getElementsByClassName("vd")[5].style.display = "none";
+		n2.getElementsByClassName("tx")[0].style.display = "block";
+		n2.getElementsByClassName("tx")[1].style.display = "none";
+		n2.getElementsByClassName("tx")[2].style.display = "none";
+		n2.getElementsByClassName("tx")[3].style.display = "none";
+		n2.getElementsByClassName("tx")[4].style.display = "none";
+		n2.getElementsByClassName("tx")[5].style.display = "none";
+	}
+	if(m===2){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "block";
+		n3.getElementsByClassName("n3")[1].style.display = "none";
+		n3.getElementsByClassName("n3")[2].style.display = "block";
+		n3.getElementsByClassName("n3")[3].style.display = "block";
+		n3.getElementsByClassName("n3")[4].style.display = "block";
+		n3.getElementsByClassName("n3")[5].style.display = "block";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "none";
+		n2.getElementsByClassName("vd")[1].style.display = "block";
+		n2.getElementsByClassName("vd")[2].style.display = "none";
+		n2.getElementsByClassName("vd")[3].style.display = "none";
+		n2.getElementsByClassName("vd")[4].style.display = "none";
+		n2.getElementsByClassName("vd")[5].style.display = "none";
+		n2.getElementsByClassName("tx")[0].style.display = "none";
+		n2.getElementsByClassName("tx")[1].style.display = "block";
+		n2.getElementsByClassName("tx")[2].style.display = "none";
+		n2.getElementsByClassName("tx")[3].style.display = "none";
+		n2.getElementsByClassName("tx")[4].style.display = "none";
+		n2.getElementsByClassName("tx")[5].style.display = "none";
+	}
+	if(m===3){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "block";
+		n3.getElementsByClassName("n3")[1].style.display = "block";
+		n3.getElementsByClassName("n3")[2].style.display = "none";
+		n3.getElementsByClassName("n3")[3].style.display = "block";
+		n3.getElementsByClassName("n3")[4].style.display = "block";
+		n3.getElementsByClassName("n3")[5].style.display = "block";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "none";
+		n2.getElementsByClassName("vd")[1].style.display = "none";
+		n2.getElementsByClassName("vd")[2].style.display = "block";
+		n2.getElementsByClassName("vd")[3].style.display = "none";
+		n2.getElementsByClassName("vd")[4].style.display = "none";
+		n2.getElementsByClassName("vd")[5].style.display = "none";
+		n2.getElementsByClassName("tx")[0].style.display = "none";
+		n2.getElementsByClassName("tx")[1].style.display = "none";
+		n2.getElementsByClassName("tx")[2].style.display = "block";
+		n2.getElementsByClassName("tx")[3].style.display = "none";
+		n2.getElementsByClassName("tx")[4].style.display = "none";
+		n2.getElementsByClassName("tx")[5].style.display = "none";
+	}
+	if(m===4){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "block";
+		n3.getElementsByClassName("n3")[1].style.display = "block";
+		n3.getElementsByClassName("n3")[2].style.display = "block";
+		n3.getElementsByClassName("n3")[3].style.display = "none";
+		n3.getElementsByClassName("n3")[4].style.display = "block";
+		n3.getElementsByClassName("n3")[5].style.display = "block";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "none";
+		n2.getElementsByClassName("vd")[1].style.display = "none";
+		n2.getElementsByClassName("vd")[2].style.display = "none";
+		n2.getElementsByClassName("vd")[3].style.display = "block";
+		n2.getElementsByClassName("vd")[4].style.display = "none";
+		n2.getElementsByClassName("vd")[5].style.display = "none";
+		n2.getElementsByClassName("tx")[0].style.display = "none";
+		n2.getElementsByClassName("tx")[1].style.display = "none";
+		n2.getElementsByClassName("tx")[2].style.display = "none";
+		n2.getElementsByClassName("tx")[3].style.display = "block";
+		n2.getElementsByClassName("tx")[4].style.display = "none";
+		n2.getElementsByClassName("tx")[5].style.display = "none";
+	}
+	if(m===5){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "block";
+		n3.getElementsByClassName("n3")[1].style.display = "block";
+		n3.getElementsByClassName("n3")[2].style.display = "block";
+		n3.getElementsByClassName("n3")[3].style.display = "block";
+		n3.getElementsByClassName("n3")[4].style.display = "none";
+		n3.getElementsByClassName("n3")[5].style.display = "block";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "none";
+		n2.getElementsByClassName("vd")[1].style.display = "none";
+		n2.getElementsByClassName("vd")[2].style.display = "none";
+		n2.getElementsByClassName("vd")[3].style.display = "none";
+		n2.getElementsByClassName("vd")[4].style.display = "block";
+		n2.getElementsByClassName("vd")[5].style.display = "none";
+		n2.getElementsByClassName("tx")[0].style.display = "none";
+		n2.getElementsByClassName("tx")[1].style.display = "none";
+		n2.getElementsByClassName("tx")[2].style.display = "none";
+		n2.getElementsByClassName("tx")[3].style.display = "none";
+		n2.getElementsByClassName("tx")[4].style.display = "block";
+		n2.getElementsByClassName("tx")[5].style.display = "none";
+	}
+	if(m===6){
+		n1.getElementsByClassName("n1")[0].style.display = "none";
+		n1.getElementsByClassName("n1")[1].style.display = "none";
+		n1.getElementsByClassName("n1")[2].style.display = "none";
+		n1.getElementsByClassName("n1")[3].style.display = "none";
+		n1.getElementsByClassName("n1")[4].style.display = "none";
+		n1.getElementsByClassName("n1")[5].style.display = "none";
+		n3.getElementsByClassName("n3")[0].style.display = "block";
+		n3.getElementsByClassName("n3")[1].style.display = "block";
+		n3.getElementsByClassName("n3")[2].style.display = "block";
+		n3.getElementsByClassName("n3")[3].style.display = "block";
+		n3.getElementsByClassName("n3")[4].style.display = "block";
+		n3.getElementsByClassName("n3")[5].style.display = "none";
+		n2.getElementsByClassName("n2")[0].style.display = "block";
+		n2.getElementsByClassName("vd")[0].style.display = "none";
+		n2.getElementsByClassName("vd")[1].style.display = "none";
+		n2.getElementsByClassName("vd")[2].style.display = "none";
+		n2.getElementsByClassName("vd")[3].style.display = "none";
+		n2.getElementsByClassName("vd")[4].style.display = "none";
+		n2.getElementsByClassName("vd")[5].style.display = "block";
+		n2.getElementsByClassName("tx")[0].style.display = "none";
+		n2.getElementsByClassName("tx")[1].style.display = "none";
+		n2.getElementsByClassName("tx")[2].style.display = "none";
+		n2.getElementsByClassName("tx")[3].style.display = "none";
+		n2.getElementsByClassName("tx")[4].style.display = "none";
+		n2.getElementsByClassName("tx")[5].style.display = "block";
+	}
+	if(m===7){
+		var input, filter, ul, li, a, i, n1;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown");
+        a = div.getElementsByTagName("a");
+        n1 = document.getElementById("n1");
+        for (i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                n1.getElementsByClassName("n1")[i].style.display = "block";
+                n2.getElementsByClassName("n2")[0].style.display = "none";
+                n3.getElementsByClassName("n3")[0].style.display = "none";
+		        n3.getElementsByClassName("n3")[1].style.display = "none";
+		        n3.getElementsByClassName("n3")[2].style.display = "none";
+		        n3.getElementsByClassName("n3")[3].style.display = "none";
+		        n3.getElementsByClassName("n3")[4].style.display = "none";
+		        n3.getElementsByClassName("n3")[5].style.display = "none";
+            } else {
+        n1.getElementsByClassName("n1")[i].style.display = "none";
+        n2.getElementsByClassName("n2")[0].style.display = "none";
+        n3.getElementsByClassName("n3")[0].style.display = "none";
+		n3.getElementsByClassName("n3")[1].style.display = "none";
+		n3.getElementsByClassName("n3")[2].style.display = "none";
+		n3.getElementsByClassName("n3")[3].style.display = "none";
+		n3.getElementsByClassName("n3")[4].style.display = "none";
+		n3.getElementsByClassName("n3")[5].style.display = "none";
+    }
+  }
+	}
+	}
